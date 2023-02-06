e07d2683d3e92ba4d69466227dbd771064e8eb59
scriptertoufiq <scriptertoufiq@gmail.com>
2023-02-06 23:23:42 +0600

    Header and product fetch from json file done

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,18 @@
 import React from "react";
-
-
+import logo from '../../images/Logo.svg'
+import './Header.css'
 const Header = () => {
     return (
         <div>
-            <h2>This is Header</h2>
+            <nav className='header'>
+                <img src={logo} alt="" />
+                <div>
+                    <a href="/shop">Orders</a>
+                    <a href="/review">Order Review</a>
+                    <a href="/inventory">Manage Inventory</a>
+                    <a href="/login">Login</a>
+                </div>
+            </nav>
         </div>
     );
 };
