e6f322cebf4980125a1f83926ceab1a0007e834b
scriptertoufiq <scriptertoufiq@gmail.com>
2023-02-11 16:24:15 +0600

    Product design done

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import './Shop.css'
+import Product from '../Product/Product';
 
 const Shop = () => {
     const [products,setProducts] = useState([]);
@@ -13,7 +14,10 @@ const Shop = () => {
     return (
         <div className='shop-container'>
             <div className="products-container">
-                <h3>This is for products {products.length}</h3>
+                {
+                    products.map(product =><Product 
+                        product={product}></Product>)
+                }
             </div>
             <div className="cart-container">
                 <h4>This is for order <summary></summary></h4>
