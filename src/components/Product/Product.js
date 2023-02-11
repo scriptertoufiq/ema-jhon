998690a25b03bde07eb81392e38659837603a6fb
scriptertoufiq <scriptertoufiq@gmail.com>
2023-02-11 16:44:20 +0600

    Product Click handeler done

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -3,7 +3,7 @@ import './Product.css'
 
 const Product = (props) => {
     const {name, img, seller, price, ratings,} = props.product;
-    
+    const {handleAddToCart} = props;
     return (
         <div className='product'>
             <img src={img} alt="" />
@@ -13,7 +13,7 @@ const Product = (props) => {
                 <p><small>Seller: {seller}</small></p>
                 <p><small>Ratings: {ratings} stars</small></p>
             </div>
-            <button className='btn-cart'>
+            <button onClick={() => props.handleAddToCart(props.product)}  className='btn-cart'>
                 <p>Add TO Cart</p>
             </button>
         </div>
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -9,14 +9,20 @@ const Shop = () => {
         fetch('products.json')
         .then(res=>res.json())
         .then(data => setProducts(data))
-    },[])
+    },[]);
+
+
+    const handleAddToCart = (product) => {
+        console.log(product);
+    }
 
     return (
         <div className='shop-container'>
             <div className="products-container">
                 {
                     products.map(product =><Product 
-                        product={product}></Product>)
+                        product={product}
+                        handleAddToCart={handleAddToCart}></Product>)
                 }
             </div>
             <div className="cart-container">
